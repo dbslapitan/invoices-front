cbb17d1674bc8ae782747b583c3eb47d95861451
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-09-30 04:03:59 -0400

    partial; filters

--- src/ui/filter/filter.tsx
+++ src/ui/filter/filter.tsx
@@ -8,7 +8,20 @@ export default function Filter(){
     return(
         <div className={`${style["filter"]}`}>
             <button className={`${style["filter__btn"]} ${isOpen ? style["filter__btn--rotate"] : ""}`} onClick={() => setIsOpen(!isOpen)}>Filter <span className={`${style["filter__btn--show"]}`}>by status</span></button>
-            
+            <ul className={`${style["filters"]} ${isOpen ? style["filters--show"] : ""}`}>
+                <li className={`${style["filters__list"]}`}>
+                    <input type="checkbox" name="draft" id="draft" />
+                    <label htmlFor="draft">Draft</label>
+                </li>
+                <li>
+                    <input type="checkbox" name="pending" id="pending" />
+                    <label htmlFor="pending">Pending</label>
+                </li>
+                <li>
+                    <input type="checkbox" name="paid" id="paid" />
+                    <label htmlFor="paid">Paid</label>
+                </li>
+            </ul>
         </div>
     );
 }
