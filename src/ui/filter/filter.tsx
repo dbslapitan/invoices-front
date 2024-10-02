477eb7068485431c30596ee4eef0aa404bfcd1a3
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-02 02:36:15 -0400

    add listeners for filter

--- src/ui/filter/filter.tsx
+++ src/ui/filter/filter.tsx
@@ -1,14 +1,43 @@
-import { useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import style from "./filter.module.scss";
 
 export default function Filter(){
 
     const [isOpen, setIsOpen] = useState(false);
 
+    const btnClick = (e: MouseEvent) => {
+        e.stopPropagation();
+        setIsOpen(!isOpen);
+    };
+
+    useEffect(() => {
+        const escListener = (e: KeyboardEvent) => {
+            if(e.key === "Escape"){
+                if(isOpen){
+                    setIsOpen(false);
+                }
+            }
+        };
+
+        const clickListener = () => {
+            if(isOpen){
+                setIsOpen(false);
+            }
+        };
+
+        window.addEventListener("keydown", escListener);
+        window.addEventListener("click", clickListener);
+
+        return () => {
+            window.removeEventListener("keydown", escListener);
+            window.removeEventListener("click", clickListener);
+        };
+    });
+
     return(
         <div className={`${style["filter"]}`}>
-            <button className={`${style["filter__btn"]} ${isOpen ? style["filter__btn--rotate"] : ""}`} onClick={() => setIsOpen(!isOpen)}>Filter<span className={`${style["filter__btn--show"]}`}>&nbsp; by status</span></button>
-            <ul className={`${style["filters"]} ${isOpen ? style["filters--show"] : ""}`}>
+            <button className={`${style["filter__btn"]} ${isOpen ? style["filter__btn--rotate"] : ""}`} onClick={btnClick}>Filter<span className={`${style["filter__btn--show"]}`}>&nbsp; by status</span></button>
+            <ul className={`${style["filters"]} ${isOpen ? style["filters--show"] : ""}`} onClick={btnClick}>
                 <li className={`${style["filters__list"]}`}>
                     <input type="checkbox" name="draft" id="draft" />
                     <label htmlFor="draft">Draft</label>
