a6a44448dfbe71ed1dbf80dd08f36c345aed55b9
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-05 01:51:35 -0400

    partial; start design on tablet

--- src/ui/invoice/invoice.tsx
+++ src/ui/invoice/invoice.tsx
@@ -9,7 +9,7 @@ export default function Invoice(){
             <h2 className={`${style["invoice__id"]}`}>RT3080</h2>
             <p className={`${style["invoice__due"]}`}>Due 19 Aug 2021</p>
             <p className={`${style["invoice__name"]}`}>Jensen Huang</p>
-            <p className={`${style["invoice__amount"]}`}>$1,800.90</p>
+            <p className={`${style["invoice__amount"]}`}>$ 1,800.90</p>
             <p className={`${style["invoice__status"]} ${style["invoice__status--green"]}`}> <span className={`${style["invoice__bullet"]}`}></span>Paid</p>
         </Link>
     );
