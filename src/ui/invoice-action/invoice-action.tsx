70bca8e4b08cc36f6f5472d1b37ac265abee038b
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-10 10:11:43 -0400

    change calendar to use dates on days array instead of number

--- src/ui/invoice-action/invoice-action.tsx
+++ src/ui/invoice-action/invoice-action.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MouseEvent, useState } from "react";
+import { FormEvent, MouseEvent, useState } from "react";
 import Back from "../back/back";
 import style from "./invoice-action.module.scss";
 import Image from "next/image";
@@ -10,26 +10,13 @@ import right from "../../../public/icons/icon-arrow-right.svg";
 export default function InvoiceAction(){
 
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const [ due, setDue ] = useState((new Date()));
+    const [ dueDate, setDueDate ] = useState(new Date((new Date()).getFullYear(), (new Date()).getMonth(), (new Date()).getDate()));
     const [ isDueOpen, setIsDueOpen ] = useState(false);
-    const month = months[due.getMonth()];
-    const day = due.getDate();
-    const year = due.getFullYear();
+    const [ monthToDisplay, setMonthToDisplay ] = useState(new Date(dueDate.getFullYear(), dueDate.getMonth(), 1));
     const days = [];
-    const daysOfPrevious = (new Date(year, due.getMonth(), 0)).getDate();
-    const daysOfCurrent = (new Date(year, due.getMonth() + 1, 0)).getDate();
-    const dayOfWeek = (new Date(year, due.getMonth(), 1)).getDay();
+    const dayOfWeek = monthToDisplay.getDay();
     for(let i = 0; i < 42; i++){
-        if(i < dayOfWeek){
-            days.push(daysOfPrevious - dayOfWeek + i + 1);
-        }
-        else if(i >= dayOfWeek && i <= daysOfCurrent + 1){
-            days.push(i - dayOfWeek + 1);
-        }
-        else{
-            days.push(i - daysOfCurrent - 1);
-        }
-
+        days.push(new Date(monthToDisplay.getFullYear(), monthToDisplay.getMonth(), i + 1 - dayOfWeek));
     }
 
     const clickHandler = (e: MouseEvent) => {
@@ -41,6 +28,10 @@ export default function InvoiceAction(){
 
     const calendarHandler = () => {
         setIsDueOpen(!isDueOpen);
+    };
+
+    const onCheckHandler = (e: FormEvent) => {
+        setDueDate(new Date(Number((e.target as HTMLInputElement).value)));
     };
 
     return(
@@ -86,11 +77,11 @@ export default function InvoiceAction(){
                 </div>
                 <div className={`${style["fieldset__block"]} ${style["fieldset__block--due"]}`}>
                     <label className={`${style["fieldset__label"]}`} htmlFor="to-due">Invoice Date</label>
-                    <input type="button" className={`${style["fieldset__input"]}`} id="to-due" value={`${day} ${month} ${year}`} onClick={calendarHandler}/>
+                    <input type="button" className={`${style["fieldset__input"]}`} id="to-due" value={`${dueDate.getDate()} ${months[dueDate.getMonth()]} ${dueDate.getFullYear()}`} onClick={calendarHandler}/>
                     <section className={`${style["calendar"]} ${ isDueOpen ? style["calendar--show"] : ""}`} onClick={(e) => e.stopPropagation()}>
                         <div className={`${style["calendar__head"]}`}>
                             <button className={`${style["calendar__previous"]}`}><Image src={left} alt="left carret"/></button>
-                            <h2 className={`${style["calendar__date"]}`}>Aug 2021</h2>
+                            <h2 className={`${style["calendar__date"]}`}>{`${months[monthToDisplay.getMonth()]} ${monthToDisplay.getFullYear()}`}</h2>
                             <button className={`${style["calendar__next"]}`}><Image src={right} alt="left carret"/></button>
                         </div>
                         <div className={`${style["calendar__dates"]}`}>
@@ -102,11 +93,13 @@ export default function InvoiceAction(){
                             <p className={`${style["calendar__day"]} ${style["calendar__day--padding"]}`}>Fr</p>
                             <p className={`${style["calendar__day"]} ${style["calendar__day--padding"]}`}>Sa</p>
                             {
-                                days.map((day, index) => {
+                                days.map(day => {
                                     return(
-                                        <div key={`${month}${year}${index}`} className={`${style["calendar__day"]}`}>
-                                            <input className={`${style["calendar__radio"]}`} type="radio" id={`${month}${year}${index}`} value={day} name="calendar" disabled={index < dayOfWeek || daysOfCurrent < index + 1}/>
-                                            <label className={`${style["calendar__label"]}`} htmlFor={`${month}${year}${index}`}>{day}</label>
+                                        <div key={day.getTime()} className={`${style["calendar__day"]}`}>
+                                            <input className={`${style["calendar__radio"]}`} type="radio" id={`${day.getTime()}`} value={day.getTime()} name="calendar" 
+                                            disabled={day.getTime() < monthToDisplay.getTime() || day.getTime() > (new Date((new Date(monthToDisplay.getTime())).setMonth(monthToDisplay.getMonth() + 1)).setDate(0))}
+                                            defaultChecked={dueDate.getTime() === day.getTime()} onChange={onCheckHandler}/>
+                                            <label className={`${style["calendar__label"]}`} htmlFor={`${day.getTime()}`}>{day.getDate()}</label>
                                         </div>
                                     )
                                 })
